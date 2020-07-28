a60d20bd261a6e566daa4356e6bfa1cfc6d28ee3
Chrys_Kou <39227067+chryspi@users.noreply.github.com>
2020-07-28 23:37:43 +0300

    Update e_conomies1.py

--- e_conomies1.py
+++ e_conomies1.py
@@ -1,26 +1,34 @@
-from tkinter import Tk,PhotoImage,Label,StringVar,Entry
+from tkinter import Tk,PhotoImage,Label,StringVar,Entry,Frame,OptionMenu
 import tkinter
 
 root=Tk()
 
 def labels(text):
     l=Label(root,text=text)
-    l.pack()                                  
+    l.pack()
+    l.configure(bg='pink')                                   
     return l
 
 def btn_crt():
     pass
 
 
-def init(): #window 
 
-   
-    root.title('E-conomies')  #gives the window this specific name
-    root.geometry("400x300")  #the width and height of the window
-    root.iconphoto(False,PhotoImage(file='pig_money.png')) #replaces the icon of feather that tkinter has by default anad puts this image in its place
-    root.configure(bg='pink')  #gives the window the specific background color 
-    l=labels("se spaso")                   #calls the labels function
-    l.configure(bg='purple')
+def init(): #window 
+    
+    root.title('E-conomies')                                     #gives the window this specific name
+    root.geometry("400x300")                                    #the width and height of the window
+    root.iconphoto(False,PhotoImage(file='pig_money.png'))     #replaces the icon of feather that tkinter has by default anad puts this image in its place
+    root.configure(bg='pink')                                 #gives the window the specific background color 
+    
+    l=labels("Please make a choice:")                       #calls the labels function
+
+    ch=StringVar(root)                                    #creates the choice variable of the dropdown menu
+    choices=["Balance","Deposit","Withdraw"]
+    ch.set(choices[0])                                  #sets the choice initially to "Balance"
+    popupMenu = OptionMenu(root, ch, *choices)
+    popupMenu.configure(bg='pale violet red')
+    popupMenu.pack()
     root.mainloop()
     
 
